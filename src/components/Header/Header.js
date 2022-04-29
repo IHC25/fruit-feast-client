0df1afb6c3b5858181f8a1f9b3261bf4b863ea27
arefin6 <imranhchy@gmail.com>
2022-04-29 11:28:53 +0600

    simple header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="light"
+        variant="light"
+        sticky="top"
+      >
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            Fruit Feast
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Inventory</Nav.Link>
+              <Nav.Link href="#pricing">Blogs</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
